fe1f16d9f29bc87b5b5ea095563f4b8f68c84c6a
linear <148347522+dropshotcaraz@users.noreply.github.com>
2025-05-03 23:58:28 +0700

    arline, features/adjust-crud-product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -19,8 +19,10 @@ class Product extends Model
         'category_id'
     ];
 
-    public function category(){
-        return $this->belongsTo(Category::class, 'category_id');
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
     }
+    
 
 }
